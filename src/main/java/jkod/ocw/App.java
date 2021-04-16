7181137ae27a40868dc34b04bcdef86efac6b1f6
Jitin Kodian <jitinkodian@gmail.com>
2021-04-16 08:14:49 +0530

    Initial Commit

---
+++ src/main/java/jkod/ocw/App.java
@@ -0,0 +1,13 @@
+package jkod.ocw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
